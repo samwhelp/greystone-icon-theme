3f39c1052ce5d9e8882c1ffcfa72aeceb93df58d
samwhelp <samwhelp@users.noreply.github.com>
2025-12-21 14:07:51 +0800

    fix

--- icon-archive.sh
+++ icon-archive.sh
@@ -13,8 +13,18 @@ cd icons
 
 
 ##
-## ## archive
+## ## archive / default edition
 ##
 
 #tar cJf GreyStone.tar.xz GreyStone
 tar -I 'xz -9 -T0' -cf GreyStone.tar.xz GreyStone
+
+
+
+
+##
+## ## archive / circle edition
+##
+
+#tar cJf GreyStone-Circle.tar.xz GreyStone-Circle
+tar -I 'xz -9 -T0' -cf GreyStone-Circle.tar.xz GreyStone-Circle
